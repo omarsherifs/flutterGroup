fcbc16ee343ed42406ee9d958dbd804407ed2ca3
Omar Sherif <omar.sherifs@live.com>
2024-01-31 19:47:45 +0200

    remove space

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Creativa App  ',
+      title: 'Creativa App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
